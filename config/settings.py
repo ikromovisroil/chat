e1a9c4c1fcf7b7e6aa2c0262d75ab67dda1cec2b
Sherzod <87847981+Sherzod0331@users.noreply.github.com>
2023-06-12 10:53:09 +0500

    dsczds

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure--6u2d*li@#f=5v=-=hsfwhj)xhd*t$ojvq1tu#%$@rwg%4!q&y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
